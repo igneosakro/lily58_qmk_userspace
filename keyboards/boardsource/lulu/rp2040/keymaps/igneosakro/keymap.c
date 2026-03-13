5d62718bebc4c90724e67cffbd23ecdee6c870c6
igneosakro <igneo.sakro@gmail.com>
2026-03-13 13:40:18 +0100

    hold logic

--- keyboards/boardsource/lulu/rp2040/keymaps/igneosakro/keymap.c
+++ keyboards/boardsource/lulu/rp2040/keymaps/igneosakro/keymap.c
@@ -62,3 +62,7 @@ KC_NO, KC_NO, KC_NO, KC_NO, MS_BTN1, MS_BTN3, MS_BTN2, KC_NO
 )
 
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _NUM, _NAV, _SYM);
+}
